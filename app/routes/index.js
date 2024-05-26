379977993e45b81f35afea8219432be40a7731e0
yuto ichinose <yuto.0526.sano@gmail.com>
2024-05-26 23:45:24 +0900

    feat: お問合せフォームのシステム作成

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,62 @@
-var express = require('express');
+var express = require("express");
+const mysql_connection = require("../lib/db");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "お問合せフォーム" });
 });
+
+// データ送信用のエンドポイント
+router.post("/send", async (req, res, next) => {
+  // 情報が入ってる
+  console.log(req.body);
+  const {username, email, content} = req.body
+  // nullチェック
+  if (!username || !email || !content) {
+    console.log('必要なデータが不足しています。');
+    // 「/」へ、リダイレクト
+    res.redirect('/?error=1') // エラーをのGETパラメタつける
+    return
+  }
+  let connection;
+  try {
+    // データベースと接続
+    connection = await mysql_connection();
+    // クエリ
+    const query =
+    "insert into contacts (name, email, content) values (?, ?, ?)";
+    // SQL実行
+    await connection.execute(query, [username, email, content]);
+    // リダイレクト
+    res.redirect('/?success=1')
+  } catch (error) {
+    // エラー処理
+    console.error("getUser error:", error);
+    res.redirect('/?error=2')
+  } finally {
+    // データベース接続終了
+    if (connection) connection.destroy();
+  }
+});
+
+// データ表示用
+router.get('/contacts', async (req, res, next) => {
+  let connection;
+  try {
+    connection = await mysql_connection();
+    const query =
+      'select name, email, content, posted_at from contacts';
+    const [result] = (await connection.execute(query))
+    // contacts.ejsを描画
+    res.render("contacts", {contacts: result})
+  } catch (error) {
+    console.error('getUser error:', error);
+    // 取得した時は空の配列を渡す
+    res.render("contacts", {contacts: []})
+  } finally {
+    if (connection) connection.destroy();
+  }
+})
 
 module.exports = router;
--- initdb.d/create-table.sql
+++ initdb.d/create-table.sql
@@ -1,6 +1,16 @@
 -- Windows環境で文字化け表示のために必要
 SET CHARACTER SET utf8mb4;
 
+-- お問合せテーブル
+
+create table contacts (
+    contact_id int auto_increment primary key,
+    name varchar(255) not null,
+    email varchar(255) not null,
+    content varchar(255) not null,
+    posted_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
 -- ユーザーテーブル
 create table users (
     user_id int auto_increment primary key,
